57e3a84f6d5e0105155562b25504200f3e1d501e
Replit user <>
2023-09-09 01:54:02 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -145,7 +145,7 @@ Pair * searchMap(HashMap * map,  char * key) {
 
     do {
         if (map->buckets[posicion] != NULL && map->buckets[posicion]->key != NULL) {
-            if (is_equal(map->buckets[posicion]->key, key) {
+            if (is_equal(map->buckets[posicion]->key, key)) {
 
                 map->current = posicion;
                 return map->buckets[posicion];
